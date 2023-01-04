75113c0c42376e2dfafbad23b2e81fd6867c605c
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-04 13:17:50 +0700

    [{1} #012] init todos feature

---
+++ lib/customs/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color pirmaryAppColor = const Color(0xFFFFC5F3);
+  static Color primaryAppColor2 = const Color.fromARGB(255, 190, 190, 190);
+  static Color scarletRed = const Color(0xFFFF2400);
+  static Color royalBlue = const Color(0xFF246EE9);
+  static Color darkAccent = const Color(0xFF121212);
+  static Color greyDivider = Colors.black26;
+}
