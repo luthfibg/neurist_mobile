ab602947a1f28dcdb57ae4068ed27ba139dcb1f6
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-10 10:56:47 +0700

    [{5} #025] service data api

--- server/api/service/fetch.php
+++ server/api/service/fetch.php
@@ -5,8 +5,10 @@ header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: X-Requested-With');
 header("Content-Type: application/json; charset=UTF-8");
 
-include_once "../../config/databse.php";
+include_once "../../config/database.php";
 include_once "../../data/service.php";
+
+$request = $_SERVER['REQUEST_METHOD'];
 
 $db = new Database();
 $conn = $db->connection();
@@ -17,3 +19,38 @@ $stmt = $service->fetch();
 $count = $stmt->rowCount();
 
 $response = [];
+
+if ($request == 'GET') {
+    if ($count > 0) {
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $data[] = array(
+                "service_id" => $service_id,
+                "service_name" => $service_name,
+                "service_address" => $service_address,
+                "service_phone" => $service_phone,
+            );
+        }
+        $response = array(
+            'status' => array(
+                'message' => 'success', 'code' => http_response_code(200)
+            ), 'data' => $data
+        );
+    } else {
+        http_response_code(404);
+        $response = array(
+            'status' => array(
+                'message' => 'no data found', 'code' => http_response_code()
+            )
+        );
+    }
+} else {
+    http_response_code(405);
+    $response = array(
+        'status' => array(
+            'message' => 'method not allowed', 'code' => http_response_code()
+        )
+    );
+}
+
+echo json_encode($response);
--- server/api/service/update.php
+++ server/api/service/update.php
@@ -16,6 +16,59 @@ $conn = $db->connection();
 $service = new Service($conn);
 
 $data = json_decode(file_get_contents("php://input"));
-$service->id = $data->id;
+$service->service_id = $data->service_id;
 
 $response = [];
+
+if ($request == 'PUT') {
+    if (
+        !empty($data->service_id) &&
+        !empty($data->service_name) &&
+        !empty($data->service_address) &&
+        !empty($data->service_phone)
+    ) {
+        $service->service_id = $data->service_id;
+        $service->service_name = $data->service_name;
+        $service->service_address = $data->service_address;
+        $service->service_phone = $data->service_phone;
+
+        $data = array(
+            'service_id' => $service->service_id,
+            'service_name' => $service->service_name,
+            'service_address' => $service->service_address,
+            'service_phone' => $service->service_phone,
+        );
+
+        if ($service->update()) {
+            $response = array(
+                'status' => array(
+                    'message' => 'success', 'code' => (http_response_code(200))
+                ), 'data' => $data
+            );
+        } else {
+            http_response_code(400);
+            $response = array(
+                'message' => 'update failed',
+                'code' => http_response_code()
+            );
+        }
+    } else {
+        http_response_code(400);
+        $response = array(
+            'status' => array(
+                'message' => 'update failed - wrong parameter',
+                'code' => http_response_code()
+            )
+        );
+    }
+} else {
+    http_response_code(405);
+    $response = array(
+        'status' => array(
+            'message' => 'method not allowed',
+            'code' => http_response_code()
+        )
+    );
+}
+
+echo json_encode($response);
