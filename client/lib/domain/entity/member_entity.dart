5da3cab0b717d3c4e48b9fac13068e19adce1911
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-11 22:34:43 +0700

    [{5} #027] client side

---
+++ client/lib/domain/entity/member_entity.dart
@@ -0,0 +1,19 @@
+class MemberEntity {
+  const MemberEntity({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.address,
+    required this.serviceId,
+    required this.phone,
+    required this.email,
+  });
+
+  final String id;
+  final String name;
+  final String age;
+  final String address;
+  final String serviceId;
+  final String phone;
+  final String email;
+}
