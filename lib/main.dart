ce95df97d44607d2511057f6e602f6d6195224d0
luthfibg <luthfinewzoneonline@gmail.com>
2023-01-13 15:34:54 +0700

    [{5} #030] server device data done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-// import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
-// import 'package:double_back_to_close/double_back_to_close.dart';
 // import 'package:neurist_mobile/views/todos.dart';
-// import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
-// import './customs/theme.dart';
 // import './views/home.dart';
 
 // void main() {
@@ -95,6 +91,11 @@
 // }
 
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:double_back_to_close/double_back_to_close.dart';
+import 'package:neurist_mobile/presentation/pages/device_page.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import './customs/theme.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neurist_mobile/presentation/blocs/member_bloc/member_bloc.dart';
 import 'package:neurist_mobile/presentation/pages/main_page.dart';
@@ -123,9 +124,79 @@ class Neurist extends StatelessWidget {
             ),
         ),
       ],
-      child: const MaterialApp(
-        home: MainPage(),
+      child: MaterialApp(
+        theme: ThemeData.dark(),
+        home: const DoubleBack(
+          child: MainPage(),
+        ),
       ),
     );
   }
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  final PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      confineInSafeArea: true,
+      screens: screens(),
+      items: navBarItems(),
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset: true,
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(0.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      itemAnimationProperties: const ItemAnimationProperties(
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle: NavBarStyle.style9,
+      backgroundColor: AppColors.darkAccent,
+    );
+  }
+
+  List<Widget> screens() {
+    return [
+      const MainPage(),
+      const DevicePage(),
+    ];
+  }
+}
+
+List<PersistentBottomNavBarItem> navBarItems() {
+  return [
+    PersistentBottomNavBarItem(
+      icon: const Icon(CupertinoIcons.home),
+      iconSize: 22,
+      textStyle: const TextStyle(fontSize: 12),
+      title: 'Home',
+      activeColorPrimary: AppColors.primaryAppColor3,
+      inactiveColorPrimary: AppColors.primaryAppColor2,
+    ),
+    PersistentBottomNavBarItem(
+      icon: const Icon(CupertinoIcons.list_bullet),
+      iconSize: 22,
+      textStyle: const TextStyle(fontSize: 12),
+      title: 'Todo',
+      activeColorPrimary: AppColors.primaryAppColor3,
+      inactiveColorPrimary: AppColors.primaryAppColor2,
+    ),
+  ];
 }
---
+++ lib/presentation/pages/device_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:neurist_mobile/presentation/blocs/device_bloc/device_bloc.dart';
+// import 'package:neurist_mobile/presentation/widgets/device_card_widget.dart';
+
+class DevicePage extends StatelessWidget {
+  const DevicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Search',
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                child: BlocBuilder<DeviceBloc, DeviceState>(
+                  builder: (context, state) {
+                    if (state is DeviceFetchedState) {
+                      return GridView.count(
+                        scrollDirection: Axis.vertical,
+                        childAspectRatio:
+                            (MediaQuery.of(context).size.height / 2) /
+                                (MediaQuery.of(context).size.width / 2 * 2),
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 15,
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        children: state.listDevice
+                            .map((device) => DeviceCardWidget(device: device))
+                            .toList(),
+                      );
+                    }
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
