ec62100c11b99f0275a7dcb239da904e59b66034
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-09 20:45:54 +0700

    [{10} #46] service repository improve data consistency

--- lib/domain/repositories/service_repository.dart
+++ lib/domain/repositories/service_repository.dart
@@ -2,11 +2,11 @@ import 'package:neurist_mobile/data/models/service_model.dart';
 
 abstract class ServiceRepository {
   Future<List<ServiceModel>> fetch();
-  // Future<List<ServiceModel>> get({
-  //   int? serviceId,
-  //   String? serviceName,
-  //   String? serviceAddress,
-  //   String? servicePhone,
-  // });
+  Future<List<ServiceModel>> get({
+    int? serviceId,
+    String? serviceName,
+    String? serviceAddress,
+    String? servicePhone,
+  });
   Future<bool> delete({required int serviceId});
 }
