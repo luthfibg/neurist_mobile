d6747cb9153e1b48407ed9a8b4613e1f8928cd74
luthfibg <luthfinewzoneonline@gmail.com>
2023-02-10 17:40:36 +0700

    [{11} #53] member entity

--- lib/domain/entity/member_entity.dart
+++ lib/domain/entity/member_entity.dart
@@ -1,1 +1,19 @@
+class MemberEntity {
+  const MemberEntity({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.address,
+    required this.serviceId,
+    required this.phone,
+    required this.email,
+  });
 
+  final int id;
+  final String name;
+  final int age;
+  final String address;
+  final int serviceId;
+  final String phone;
+  final String email;
+}
